6d08bfc3be8431293a5053eb0a6071c6d629d048
nnoose <91214665+nnoose@users.noreply.github.com>
2022-06-02 14:56:31 -0700

    Implemented basic structure of Parser.java

--- cs350s22/component/ui/parser/Parser.java
+++ cs350s22/component/ui/parser/Parser.java
@@ -11,16 +11,56 @@ public class Parser {
     }
 
     public void parse() throws IOException {
-        System.out.println("PARSING: " + commandText);
-        if(commandText.equalsIgnoreCase("@exit"))
+
+        /*if(commandText.equalsIgnoreCase("@exit"))
             parserHelper.exit();
         if(commandText.matches("^CREATE ACTUATOR.+") || commandText.matches("^CREATE SENSOR.+") || commandText.matches("^BUILD NETWORK.+"))
         {
             KeithParser k = new KeithParser((ParserHelper) parserHelper, commandText);
             k.parse();
+        }*/
+        String[] tokens = commandText.toUpperCase().split(" ");
+        switch(tokens[0])
+        {
+            case "CREATE":
+                if(tokens.length < 2)
+                    throw new IllegalArgumentException("Malformed command:" + System.lineSeparator() + commandText);
+                switch(tokens[1])
+                {
+                    case "ACTUATOR":
+                    case "SENSOR":
+                        KeithParser k = new KeithParser((ParserHelper) parserHelper, commandText);
+                        k.parse();
+                        break;
+                        //Other CREATE Commands can go under here
+                    case "REPORTER":
+                    case "WATCHDOG":
+                        break; //insert Hieu Tran's solution here.
+
+                    case "MAPPER":
+                        break; //insert Alex Cannon's solution here.
+                    default:
+                        throw new IllegalArgumentException("Malformed command:" + System.lineSeparator() + commandText);
+                }
+                break;
+            case "BUILD":
+                KeithParser k = new KeithParser((ParserHelper) parserHelper, commandText);
+                k.parse();
+                break;
+            case "SEND": //TBD
+                break;
+
+            case "@CLOCK": //In progress
+                break;
+            case "@EXIT":
+                parserHelper.exit();
+                break;
+            case "@RUN": //In progress
+                break;
+            case "@CONFIGURE"://TBD
+                break;
+            default:
+                throw new IllegalArgumentException("Malformed command:" + System.lineSeparator() + commandText);
         }
-
-
-
     }
 }
