82431eafe1838388c2416d3644245970a86d1a28
Hieu Tran <htran9@ewu.edu>
2022-06-09 15:53:59 -0700

    Reporter and Watchdog implemeneted. Still need to do testing/debugging.

--- cs350s22/component/ui/parser/Parser.java
+++ cs350s22/component/ui/parser/Parser.java
@@ -11,19 +11,20 @@ import java.io.IOException;
 public class Parser {
     private A_ParserHelper parserHelper;
     private String commandText;
+    private String [] tokens;
     public Parser(final A_ParserHelper parserHelper, final String commandText) {
         this.commandText = commandText;
         this.parserHelper = parserHelper;
+        tokens = commandText.split(" ");
     }
 
     public void parse() throws IOException, ParseException {
-        String[] tokens = commandText.split(" ");
-        HieuParser hieuParser = new HieuParser(parserHelper, commandText);
+
         switch(tokens[0].toUpperCase())
         {
             case "CREATE":
                 if(tokens.length < 2)
-                    throw new IllegalArgumentException("Malformed command:" + System.lineSeparator() + commandText);
+                    throw new RuntimeException("Malformed command:" + System.lineSeparator() + commandText);
                 switch(tokens[1].toUpperCase())
                 {
                     case "ACTUATOR":
@@ -31,19 +32,19 @@ public class Parser {
                         KeithParser k = new KeithParser((ParserHelper) parserHelper, commandText);
                         k.parse();
                         break;
-                        //Other CREATE Commands can go under here
+                    //Other CREATE Commands can go under here
                     case "REPORTER":
-                        hieuParser.parse();
                     case "WATCHDOG":
+                        HieuParser hieuParser = new HieuParser(parserHelper, commandText);
                         hieuParser.parse();
-                        break; //insert Hieu Tran's solution here.
+                        break;
 
                     case "MAPPER":
                         AlexParser a = new AlexParser((ParserHelper)parserHelper, commandText);
                         a.parse();
                         break;
                     default:
-                        throw new IllegalArgumentException("Malformed command:" + System.lineSeparator() + commandText);
+                        throw new RuntimeException("Malformed command:" + System.lineSeparator() + commandText);
                 }
                 break;
             case "BUILD":
@@ -52,50 +53,21 @@ public class Parser {
                 break;
             case "SEND": //TBD
                 break;
-
-            case "@CLOCK": //In progress
-                if(tokens.length < 2)
-                    throw new IllegalArgumentException("Malformed command:" + System.lineSeparator() + commandText);
-                Clock clock = Clock.getInstance();
-                switch(tokens[1].toUpperCase())
-                {
-                    case "PAUSE":
-                        clock.isActive(false);
-                        break;
-                    case "RESUME":
-                        clock.isActive(true);
-                        break;
-                    case "ONESTEP":
-                        if(tokens.length > 2)
-                        {
-                            int count = Integer.parseInt(tokens[2]);
-                            clock.onestep(count);
-                        }
-                        else clock.onestep();
-                        break;
-                    case "SET":
-                        if(tokens.length < 4||!tokens[2].equalsIgnoreCase("RATE"))
-                            throw new IllegalArgumentException("Malformed command:" + System.lineSeparator() + commandText);
-
-                        int multiplier = Integer.parseInt(tokens[3]);
-                        clock.setRate(multiplier);
-                        break;
-                    default:
-                        throw new IllegalArgumentException("Malformed command:" + System.lineSeparator() + commandText);
-                }
+            case "@CLOCK":
+                parseClock();
                 break;
             case "@EXIT":
                 parserHelper.exit();
                 break;
-            case "@RUN": //In progress
+            case "@RUN":
                 String filename = tokens[2].replaceAll("\"","");
                 parserHelper.run(filename);
                 break;
-            case "@CONFIGURE"://In progress
+            case "@CONFIGURE":
             {
 
                 if (tokens.length < 7)
-                    throw new IllegalArgumentException("Malformed command:" + System.lineSeparator() + commandText);
+                    throw new RuntimeException("Malformed command:" + System.lineSeparator() + commandText);
 
                 String log = tokens[2].replaceAll("\"","");
                 String dot = tokens[5].replaceAll("\"","");
@@ -103,11 +75,43 @@ public class Parser {
                 String xml = tokens[9].replaceAll("\"","");
                 LoggerMessage.initialize(Filespec.make(log));
                 LoggerMessageSequencing.initialize(Filespec.make(dot), Filespec.make(network));
-                LoggerActuator.initialize(Filespec.make(xml));
             }
             break;
             default:
-                throw new IllegalArgumentException("Malformed command:" + System.lineSeparator() + commandText);
+                throw new RuntimeException("Malformed command:" + System.lineSeparator() + commandText);
+        }
+    }
+
+    private void parseClock()
+    {
+        if(tokens.length < 2)
+            throw new RuntimeException("Malformed command:" + System.lineSeparator() + commandText);
+        Clock clock = Clock.getInstance();
+        switch(tokens[1].toUpperCase())
+        {
+            case "PAUSE":
+                clock.isActive(false);
+                break;
+            case "RESUME":
+                clock.isActive(true);
+                break;
+            case "ONESTEP":
+                if(tokens.length > 2)
+                {
+                    int count = Integer.parseInt(tokens[2]);
+                    clock.onestep(count);
+                }
+                else clock.onestep();
+                break;
+            case "SET":
+                if(tokens.length < 4||!tokens[2].equalsIgnoreCase("RATE"))
+                    throw new RuntimeException("Malformed command:" + System.lineSeparator() + commandText);
+
+                int multiplier = Integer.parseInt(tokens[3]);
+                clock.setRate(multiplier);
+                break;
+            default:
+                throw new RuntimeException("Malformed command:" + System.lineSeparator() + commandText);
         }
     }
 }
