f7aaafcc7831a33b1f858d8c54ba8f5c5683d888
nnoose <91214665+nnoose@users.noreply.github.com>
2022-06-14 21:10:20 -0700

    moved startup to the correct package

--- cs350s22/startup/Startup.java
+++ cs350s22/startup/Startup.java
@@ -1,3 +1,5 @@
+package cs350s22.startup;
+
 import cs350s22.component.ui.parser.A_ParserHelper;
 import cs350s22.component.ui.parser.Parser;
 import cs350s22.component.ui.parser.ParserHelper;
@@ -20,23 +22,27 @@ public class Startup
       
       // this command must come first. The filenames do not matter here
       startup.parse("@CONFIGURE LOG \"a.txt\" DOT SEQUENCE \"b.txt\" NETWORK \"c.txt\" XML \"d.txt\"");
-      startup.parse("@CLOCK PAUSE");
-      startup.parse("@CLOCK ONESTEP 20");
-      startup.parse("@CLOCK ONESTEP");
+
 
       // Watchdog commands.
       startup.parse("CREATE WATCHDOG BAND myWatchdog1 MODE INSTANTANEOUS THRESHOLD LOW 1 HIGH 3");
       startup.parse("CREATE WATCHDOG ACCELERATION myWatchdog2 MODE INSTANTANEOUS THRESHOLD LOW 1 HIGH 3");
       startup.parse("CREATE WATCHDOG LOW myWatchdog3 MODE AVERAGE THRESHOLD 3 GRACE 4");
 
-      startup.parse("CREATE SENSOR POSITION mySensor");
-      startup.parse("CREATE ACTUATOR ROTARY myac SENSOR mySensor ACCELERATION LEADIN 10 LEADOUT -5 RELAX 20 VELOCITY LIMIT 100 VALUE MIN -1000 MAX 1000 INITIAL 500 JERK LIMIT 20"); //test actuator
-      startup.parse("BUILD NETWORK WITH COMPONENT myac");
+
 
       // Reporter commands.
       startup.parse("CREATE REPORTER CHANGE myReporter1 NOTIFY IDS myActuator1 myActuator2 DELTA 3");
       startup.parse("CREATE REPORTER FREQUENCY myReporter6 NOTIFY IDS myActuator1 myActuator2 GROUPS myGroup3 FREQUENCY 4");
 
+      startup.parse("CREATE SENSOR POSITION mySensor REPORTERS myReporter1 myReporter6 WATCHDOGS myWatchdog1 myWatchdog2 myWatchdog3");
+      startup.parse("CREATE ACTUATOR ROTARY myac SENSOR mySensor ACCELERATION LEADIN 10 LEADOUT -5 RELAX 20 VELOCITY LIMIT 100 VALUE MIN -1000 MAX 1000 INITIAL 500 JERK LIMIT 20"); //test actuator
+      startup.parse("BUILD NETWORK WITH COMPONENT myac");
+
+
+      startup.parse("@CLOCK PAUSE");
+      startup.parse("@CLOCK ONESTEP 20");
+      startup.parse("@CLOCK ONESTEP");
       // run your tests like this
       startup.parse("@exit");
    }
